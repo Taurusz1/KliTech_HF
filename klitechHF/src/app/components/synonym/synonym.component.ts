13c6cc264aa1bf179cced64dd558565f91de95e0
Taurusz1 <nyiribalazsszabolcs@gmail.com>
2023-05-15 13:24:26 +0200

    minor fixes

--- klitechHF/src/app/components/synonym/synonym.component.ts
+++ klitechHF/src/app/components/synonym/synonym.component.ts
@@ -5,7 +5,7 @@ import { SynonymService } from 'src/app/services/synonym.service';
 @Component({
   selector: 'app-synonym',
   templateUrl: './synonym.component.html',
-  styleUrls: ['./synonym.component.css']
+  styleUrls: ['./synonym.component.css'],
 })
 
 /*Implements onSubmitSyn function uses the synonymSearch variable and calls the getSynonyms function of the Service
@@ -16,15 +16,15 @@ export class SynonymComponent {
   synonymData?: SynonymData;
 
   //Recieves a SynonymService parameter to be used with the onSubmit function
-  constructor(
-    private synonymService: SynonymService
-  ) { }
+  constructor(private synonymService: SynonymService) {}
 
   ngOnInit(): void {}
-  
+
   //Sends the http request through the service, and in the next() saves the data to the model object
   onSubmitSyn() {
-    this.synonymService.getSynonyms(this.synonymSearch).subscribe({
+    const trimmed = this.synonymSearch.trim();
+    const word = trimmed.split(' ');
+    this.synonymService.getSynonyms(word[0]).subscribe({
       next: (response) => {
         this.synonymData = response;
       },
